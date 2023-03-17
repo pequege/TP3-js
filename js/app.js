7fd8e69b170e4bc27d45017b66d1c2ecff566f2d
Gerardo Mansilla <gerardoemansilla@gmail.com>
2023-03-17 14:42:52 -0300

    agrego ejercicio 2 y 3

--- js/app.js
+++ js/app.js
@@ -5,3 +5,70 @@ function ejercicio1(){
     });
 }
 
+function ejercicio2(){;
+    let paises = []
+    let opc = true;
+    let pais;
+    do {
+        let pais = prompt("Ingrese un país");
+        opc = confirm("¿Agregar país?");
+        paises.push(pais)
+    } while (opc);
+    paises.forEach(pais => {
+        document.write(` ${pais} <br>`);
+    });
+}
+
+function ejercicio3(){
+    let resultados =[];
+
+    function lanzarDados(){
+        let dado1 = Math.floor(Math.random() * 6) + 1;
+        let dado2 = Math.floor(Math.random() * 6) + 1;
+        return dado1 + dado2;
+    }
+    
+    function lanzarDadosNVeces(n){
+        for(let i = 0; i < n; i++){
+            resultados.push(lanzarDados());
+        }
+    }
+
+    function contarOcurrencias(arr,n){
+        let ocurrencias = 0;
+        arr.forEach(element => {
+            if(element === n){ocurrencias++};
+        });
+        return ocurrencias;
+    }
+    
+    function graficarResultados(arr){
+        /* document.write(`<table class="table table-striped-columns">
+	    <tbody>`);
+        for (let i = 2; i <= 12; i++) {
+            document.write(`<tr>`);//dibuja fila
+            document.write(`<td>${i}</td>`);
+            document.write(`<td>${contarOcurrencias(arr, i)}</td>`);
+            document.write(`</tr>`);
+        }
+        document.write(`</tbody>
+        </table>`); */
+        let html_partial = (`<table class="table table-striped-columns">
+            <tbody>`);
+        for (let i = 2; i <= 12; i++) {
+            html_partial += (`<tr>`);
+            html_partial += (`<td>${i}</td>`);
+            html_partial += (`<td>${contarOcurrencias(arr, i)}</td>`);
+            html_partial += (`</tr>`);
+            }
+        html_partial += (`</tbody>
+            </table>`);
+        document.getElementById("ejercicio3paragraph").innerHTML = html_partial;
+    }
+    
+    lanzarDadosNVeces(500);
+    graficarResultados(resultados);
+    console.log(resultados);
+
+}
+
