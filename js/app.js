4a4ab9d5a40076f8047bef81aea091afa7042d91
Gerardo Mansilla <gerardoemansilla@gmail.com>
2023-03-17 13:33:13 -0300

    agrego estructura principal y ejercicio 1

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function ejercicio1(){
+    const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Setiempre", "Octubre", "Noviembre", "Diciembre"];
+    meses.forEach(mes => {
+        document.write(`${mes}<br>`);
+    });
+}
+
